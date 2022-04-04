369db95217772f9f6f6e030a30656b31bea9d451
Patrick Pedersen <patrick.pedersen@nav.no>
2022-04-04 20:28:31 +0200

    Configure project settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://navikt.github.io',
+  baseUrl: '/dbt-i-nav/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'navikt', // Usually your GitHub org/user name.
+  projectName: 'dbt-i-nav', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
